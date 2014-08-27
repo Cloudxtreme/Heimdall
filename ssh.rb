8b6c6d638167dcf4a520a18bbc64ad78254394c5
Mike Canann <Michael.Canann@lendkey.com>
2014-08-26 20:33:08 -0400

    adding yaml file to hold server settings

--- ssh.rb
+++ ssh.rb
@@ -1,5 +1,8 @@
 require 'java_imports.rb'
 
+require 'yaml'
+require 'thread'
+
 class TopFrame < JFrame
 
   def initialize
@@ -11,6 +14,45 @@ class TopFrame < JFrame
 
   def init_ui_components()
 
+    # default linux setting, uses the keys in the user's .ssh directory
+    @identity_file = ""
+
+    # use putty on windows, gnome on linux and Terminal on mac
+    @islinux = RUBY_PLATFORM.downcase.include?("linux")
+    @ismac = RUBY_PLATFORM.downcase.include?("darwin")
+
+    @connect_cmd = '"c:\Program Files\PuTTY\putty.exe" '
+    if @islinux
+       @connect_cmd = "gnome-terminal --execute ssh "
+    elsif @ismac
+       @connect_cmd = `osascript -e 'tell application "Terminal" to do script "ssh xxx"'`
+    end
+
+    # save the list of windows created, so garbage collection doesn't kill
+    @save_sub_process = Array.new
+
+
+    @config_file_yaml = "#{File.expand_path(File.dirname(__FILE__))}/settings.yaml"
+
+    puts @config_file_yaml
+
+    if File.exist? @config_file_yaml
+      @yaml_data = YAML.load_file( @config_file_yaml )
+    else
+      puts "yaml file missing"
+      exit
+    end
+
+
+
+    if (!@islinux && !@ismac)
+      if @yaml_data['win_ssh_key'].length > 1
+        @identity_file = " -i #{@yaml_data['win_ssh_key']} "
+      end
+    end
+
+
+
     contentPane = getContentPane()
     gridbag = GridBagLayout.new
     contentPane.setLayout gridbag
@@ -20,19 +62,6 @@ class TopFrame < JFrame
     #setting a default constraint value
     grid.fill = GridBagConstraints::HORIZONTAL
 
-#    cb = JCheckBox.new "Show Title", true
-#    cb.setBounds 50, 60, 80, 30
-#    cb.setFocusable(false)
-#
-#    cb.add_action_listener do |e|
-#      if self.getTitle.empty?
-#        self.setTitle "JCheckBox example"
-#      else
-#        self.setTitle ""
-#      end
-#    end
-#
-#    add cb
 
     siteLbl = JLabel.new("Site:")
     grid.gridx = 0
@@ -53,10 +82,9 @@ class TopFrame < JFrame
     contentPane.add(userLbl)
 
     siteCombo = JComboBox.new
-    siteCombo.addItem "development"
-    siteCombo.addItem "alpha"
-    siteCombo.addItem "beta"
-    siteCombo.addItem "production"
+    @yaml_data['sessions'].each_index do |i|
+      siteCombo.addItem "#{@yaml_data['sessions'][i][0]['site']}"
+    end
     grid.gridx = 1
     grid.gridy = 0
     grid.gridwidth = 1
@@ -97,6 +125,7 @@ class TopFrame < JFrame
     # don't set the size, let swing figure it out
     #set_preferred_size(Dimension.new(275, 175))
   end
+
 
 end
 
