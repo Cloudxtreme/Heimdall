b1174091c4dc9ae782a7abdc3abe475c48b3e7fa
Mike Canann <Michael.Canann@lendkey.com>
2014-08-26 22:08:42 -0400

    filling out the UI based on the yaml

--- ssh.rb
+++ ssh.rb
@@ -69,7 +69,7 @@ class TopFrame < JFrame
     gridbag.setConstraints(siteLbl, grid)
     contentPane.add(siteLbl)
 
-    descLbl = JLabel.new("Description:")
+    descLbl = JLabel.new("host:")
     grid.gridx = 0
     grid.gridy = 1
     gridbag.setConstraints(descLbl, grid)
@@ -81,50 +81,123 @@ class TopFrame < JFrame
     gridbag.setConstraints(userLbl, grid)
     contentPane.add(userLbl)
 
-    siteCombo = JComboBox.new
+    @siteCombo = JComboBox.new
     @yaml_data['sessions'].each_index do |i|
-      siteCombo.addItem "#{@yaml_data['sessions'][i][0]['site']}"
+      @siteCombo.addItem "#{@yaml_data['sessions'][i][0]['site']}"
     end
+
     grid.gridx = 1
     grid.gridy = 0
     grid.gridwidth = 1
-    gridbag.setConstraints(siteCombo, grid)
-    contentPane.add(siteCombo)
+    gridbag.setConstraints(@siteCombo, grid)
+    contentPane.add(@siteCombo)
 
-    siteCombo.add_action_listener do |e|
-      puts siteCombo.getSelectedItem()
+    @siteCombo.add_action_listener do |e|
+      puts "site changed"
+      site_change @siteCombo.getSelectedItem()
     end
 
-    serverCombo = JComboBox.new
-    serverCombo.addItem "localhost"
-    serverCombo.addItem "127.0.0.1"
+    @hostCombo = JComboBox.new
     grid.gridx = 1
     grid.gridy = 1
     grid.gridwidth = 1
-    gridbag.setConstraints(serverCombo, grid)
-    contentPane.add(serverCombo)
+    gridbag.setConstraints(@hostCombo, grid)
+    contentPane.add(@hostCombo)
 
-    userCombo = JComboBox.new
-    userCombo.addItem "mikec"
-    userCombo.addItem "root"
+    @hostCombo.add_action_listener do |e|
+      puts "host changed"
+      host_change @hostCombo.getSelectedItem()
+    end
+
+    @userCombo = JComboBox.new
     grid.gridx = 1
     grid.gridy = 2
     grid.gridwidth = 2
-    gridbag.setConstraints(userCombo, grid)
-    contentPane.add(userCombo)
+    gridbag.setConstraints(@userCombo, grid)
+    contentPane.add(@userCombo)
 
+    @userCombo.add_action_listener do |e|
+      puts "user changed"
+      user_change (@userCombo.getSelectedItem())
+    end
 
-    connectBtn = JButton.new("Connect")
+    @connectBtn = JButton.new("Connect")
     grid.gridx = 0
     grid.gridy = 3
-    gridbag.setConstraints(connectBtn, grid)
-    contentPane.add(connectBtn)
+    gridbag.setConstraints(@connectBtn, grid)
+    contentPane.add(@connectBtn)
 
     set_default_close_operation(JFrame::EXIT_ON_CLOSE)
 
     # don't set the size, let swing figure it out
     #set_preferred_size(Dimension.new(275, 175))
+
+    # fill out the server and user boxes
+    # based on the site setting
+    site_change @siteCombo.getSelectedItem()
   end
+
+  private
+
+
+  def site_change new_site
+
+    # remove all the servers for this site
+    @hostCombo.removeAllItems()
+
+    rval = ''
+    # add new items
+    @yaml_data['sessions'].each do |session|
+      if new_site == session[0]['site']
+
+        session[1].each_index do |i|
+          @hostCombo.addItem "#{session[1][i]['desc']} - #{session[1][i]['host']}"
+        end
+
+      #change the host to the first host setting
+      rval = "#{session[1][0]['desc']} - #{session[1][0]['host']}"
+      end
+    end
+
+    puts "setting host to #{rval} (#{new_site})"
+    host_change(rval)
+  end
+
+  def host_change new_host
+
+    # remove all the users for this server
+    @userCombo.removeAllItems()
+    rval = ''
+
+    @yaml_data['sessions'].each do |session|
+      if @siteCombo.getSelectedItem() == session[0]['site']
+
+        session[1].each_index do |i|
+          if new_host == "#{session[1][i]['desc']} - #{session[1][i]['host']}"
+
+            session[1][i]['users'].each_index do |j|
+              @userCombo.addItem session[1][i]['users'][j][0]['user']
+            end
+
+            #change the host to the first host setting
+            rval = session[1][i]['users'][0][0]['user']
+          end
+        end
+      end
+    end
+    puts "setting user to #{rval} (#{new_host})"
+    user_change(rval)
+  end
+
+  def user_change new_user
+
+    @user_name_data = new_user
+
+    puts "new_user"
+    puts new_user
+    puts "u"
+
+   end
 
 
 end
